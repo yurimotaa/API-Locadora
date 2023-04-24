38504d1f4f7f0357c878b2d5fdf9c00737259b83
Yuri Mota <yurirm55@gmail.com>
2023-04-24 20:38:55 -0300

    feat: cria tabela Movies

---
+++ src/migrations/1682363608909-createMoviesTable.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateMoviesTable1682363608909 implements MigrationInterface {
+    name = 'CreateMoviesTable1682363608909'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "movie" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "description" text, "duration" integer NOT NULL, "price" integer NOT NULL, CONSTRAINT "UQ_cee7125f3cbad047d34a6e13539" UNIQUE ("name"), CONSTRAINT "PK_cb3bb4d61cf764dc035cbedd422" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "movie"`);
+    }
+
+}
